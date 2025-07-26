731332a0c7f8673b3eaa3f731c0eeef9f4a357d9
Pooya Parsa <pooya@pi0.io>
2025-07-26 18:16:06 +0200

    try env

--- nitro.config.ts
+++ nitro.config.ts
@@ -25,7 +25,10 @@ export default defineNitroConfig({
   },
   vercel: {
     functions: {
-      shouldAddSourceMapSupport: true,
+      // shouldAddSourceMapSupport: true,
+      environment: {
+        NODE_OPTIONS: "--enable-source-maps",
+      },
     },
   },
 });
