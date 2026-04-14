9e22f063e8a4ca79057e73018327ed3ccd67a71e
Pooya Parsa <pooya@pi0.io>
2026-04-15 01:08:25 +0200

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import { nitro } from "nitro/vite";
 
-console.log(process.env)
+console.log(process.env);
 
 export default defineConfig({
   plugins: [nitro()],
   nitro: {
     preset: "edgeone-pages",
-        output: {
-      dir: "{{ rootDir }}/.edgeone",
-      serverDir: "{{ output.dir }}/cloud-functions/ssr-node",
+    serveStatic: false,
+    output: {
       publicDir: "{{ output.dir }}/assets/{{ baseURL }}",
     },
-  }
+  },
 });
